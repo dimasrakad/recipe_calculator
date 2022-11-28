8f561121d23bac3a2e97280b508adf97c5c86878
dimasrakad <dimasdewanggana@gmail.com>
2022-11-28 19:23:44 +0700

    recipe model

---
+++ src/modules/recipe/model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from "mongoose";
+
+// Create an interface representing a document in MongoDB.
+interface IRecipe {
+  code: string;
+  productName: string;
+  date: Date;
+  ingredients: [name: string, quantityPurchase: number, cost: number, quantityUsed: number, totalCost: number];
+  totalBatchCost: number;
+  sellingPrice: number;
+}
+
+// Create a Schema corresponding to the document interface.
+const recipeSchema = new Schema<IRecipe>({
+  code: { type: String, required: true },
+  productName: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  ingredients: { type: Array, required: true },
+  totalBatchCost: { type: Number, required: true },
+  sellingPrice: { type: Number, required: true },
+});
+
+// Create a Model.
+const Recipe = model<IRecipe>("Recipe", recipeSchema);
+
+export default Recipe;
